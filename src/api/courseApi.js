3c886080b1c9e71eb035a16b1f21745978c3246b
Saurabh <saurabhbharti9@gmail.com>
2020-03-15 02:54:06 +0530

    added course page

--- src/api/courseApi.js
+++ src/api/courseApi.js
@@ -1,42 +1,42 @@
-import {handleResponse,handleError} from './apiUtils';
-const baseUrl=process.env.REACT_APP_API_URL +"/courses/";
+import { handleResponse, handleError } from "./apiUtils";
+const baseUrl = process.env.REACT_APP_API_URL + "/courses/";
 
-export function getCourses(){
-    return fetch(baseUrl)
+export function getCourses() {
+  return fetch(baseUrl)
     .then(handleResponse)
-    .then(handleError);
+    .catch(handleError);
 }
 
-export function getCourseBySlug(slug){
-    return fetch(baseUrl+"?slug="+slug)
-    .then(response=>{
-        if(!response.ok) throw new Error("Network response was not Ok");
-        return response.json().then(courses=>{
-            if(courses.length!==1) throw new Error("Course not found"+slug);
-            return courses[0];//should only find one course for a given slug, so return it.
-        })
+export function getCourseBySlug(slug) {
+  return fetch(baseUrl + "?slug=" + slug)
+    .then(response => {
+      if (!response.ok) throw new Error("Network response was not Ok");
+      return response.json().then(courses => {
+        if (courses.length !== 1) throw new Error("Course not found" + slug);
+        return courses[0]; //should only find one course for a given slug, so return it.
+      });
     })
     .catch(handleError);
 }
 
-export function saveCourse(course){
-    return fetch(baseUrl+(course.id||""),{
-        method:course.id?"PUT":"POST",
-        headers:{"content-type":"application/json"},
-        body:JSON.stringify({
-            ...course,
-            //parse to int if ID is passed as sting
-            authorID:parseInt(course.authorID,10)
-        })
+export function saveCourse(course) {
+  return fetch(baseUrl + (course.id || ""), {
+    method: course.id ? "PUT" : "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      ...course,
+      //parse to int if ID is passed as sting
+      authorID: parseInt(course.authorID, 10)
     })
+  })
     .then(handleResponse)
     .then(handleError);
 }
 
-export function deleteCourse(courseId){
-    return fetch(baseUrl+courseId,{
-        method:"DELETE",
-    })
+export function deleteCourse(courseId) {
+  return fetch(baseUrl + courseId, {
+    method: "DELETE"
+  })
     .then(handleResponse)
     .then(handleError);
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "./common/Header";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import CoursesPage from "./CoursesPage";
 
 function App() {
   function getPage() {
     const route = window.location.pathname;
+    if (route === "/courses") return <CoursesPage />;
     if (route === "/about") return <AboutPage />;
     else return <HomePage />;
   }
