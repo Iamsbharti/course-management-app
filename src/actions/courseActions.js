a9c1ef5218ef3dda33a0d6e19075c3dc3ecc5b5e
Saurabh <saurabhbharti9@gmail.com>
2020-03-18 01:49:51 +0530

    implemented flux-actions

---
+++ src/actions/courseActions.js
@@ -0,0 +1,13 @@
+import dispatcher from "../appDispatcher";
+import actionTypes from "./actionTypes";
+import * as courseApi from "../api/courseApi";
+
+export function saveCourse(course) {
+  return courseApi.saveCourse(course).then(savedCourse => {
+    //Hey dispatcher tell all the stores that a course was created
+    dispatcher.dispatch({
+      actionType: actionTypes.CREATE_COURSE,
+      course: savedCourse
+    });
+  });
+}
