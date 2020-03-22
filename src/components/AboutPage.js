654f20b7698ebc9b2175ba027ef36269d2dd7547
Saurabh <saurabhbharti9@gmail.com>
2020-03-22 19:59:48 +0530

    modified about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -4,6 +4,16 @@ function AboutPage() {
   return (
     <div>
       <h2>About Page</h2>
+      <p>This is a react-app creted with help of create-react-app project.</p>
+      <p>It supports following functionalities</p>
+      <ul>
+        <li>
+          Display Course Details from a mock database by making an API-call to
+          JSON Server
+        </li>
+        <li>Add & Delete an additional course</li>
+        <li>Redirect to 404 Page when incorrect URL is entered</li>
+      </ul>
     </div>
   );
 }
